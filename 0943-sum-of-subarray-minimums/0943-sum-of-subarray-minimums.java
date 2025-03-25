ab06e443d241017eff1f07eaa65437a07d312af4
antara316 <130331799+antara316@users.noreply.github.com>
2025-03-25 11:00:49 +0530

    Time: 73 ms (31.61%), Space: 51 MB (57.31%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,44 +1,55 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int length = arr.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-      
-        Arrays.fill(left, -1);
-        Arrays.fill(right, length);
-      
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
+        int nse[] = findnse(arr);
+        int psee[] = findpsee(arr);
+        long total = 0 ;
+        int mod = (int) 1e9 +7;
+        int n = arr.length;
+        for(int i = 0 ; i<= n-1 ; i++)
+        {
+            int left = i- psee[i];
+           int  right = nse[i] - i;
+            total = (total + (long)(left * right * (long)arr[i])% mod)% mod;
         }
-      
-        stack.clear();
-      
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
+
+        return (int)total;
+    }
+
+    public static int [] findnse(int arr[])
+    {
+        Stack <Integer> st = new Stack<>();
+        int n = arr.length;
+        int nse[] = new int[n];
+
+        for(int i = n-1 ; i>= 0 ; i--)
+        {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i])
+            {
+                st.pop();
             }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
+
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        return nse;
+    }
+
+    public static int[] findpsee(int arr[])
+    {
+        int n = arr.length;
+        int psee[] = new int[n];  
+        Stack<Integer> st = new Stack<>();
+        for(int i=0 ; i<= n-1 ; i++)
+        {
+            while(!st.isEmpty() && arr[st.peek()]> arr[i])
+            {
+                st.pop();
             }
-            stack.push(i);
-        }
-      
-        int mod = (int) 1e9 + 7;
-        long answer = 0;
-      
-        for (int i = 0; i < length; ++i) {
-            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
-            answer %= mod;
-        }
-      
-        return (int) answer;
+            psee[i] = st.isEmpty()? -1 : st.peek();
+            st.push(i);
+        } 
+
+        return psee;
     }
 }
